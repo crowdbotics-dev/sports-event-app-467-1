37a16c9972c5aa253d7cc1893b8c9666ae67df05
Crowdbotics <engineering@crowdbotics.com>
2024-04-01 19:33:17 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sports-event-app-467-1",
+  url: "https://sports-event-app-467-1.botics.co",
+  api: "https://sports-event-app-467-1.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
